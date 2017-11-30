71cec42508c752d29ce4fd88df5fe4fcd3be9c01
Jose Andres <benjamin.wwe@hotmail.es>
2017-11-29 22:12:58 -0600

    Fixed portability issues

--- MPyC.c
+++ MPyC.c
@@ -1,46 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <cstdlib>
 #include <time.h>
-
-
-void menuprin();
-void creditos();
-
-main(){
-	creditos();
-	
-}
-
-void creditos(){
-	char c=' ';
-	printf("SERPIENTES Y ESCALERAS DELUXE EDITION SEASON PASS");
-	printf("\nCreado por:\nJorge Goytia\nAndres Lucatero\nDiego Mendez\nCassiel Ortega\nGabriel Villanueva");
-	printf("\n\n\n\n2017(c)\nPresiona cualquier tecla para continuar");
-	while(c==' '){
-		scanf("%c",&c);		
-	}
-	menuprin();
-}
-
-
 
 void menuprin(){
 	char opc=' ';
-	bool band=true;
-	while(band==true){
+	int band=1;
+	while(band==1){
 		fflush(stdin);
-		printf("\nSelecciona una opcion: \n1.-Jugar\n2.-Estadisticas\n3.-Salir\n");
+		printf("\nSelecciona una opcion: \n1.-Entrar\n2.-Registrar\n3.-Salir\n");
 		scanf("%c",&opc);
 		fflush(stdin);
 		if(opc=='1'){
-			main();
+			int main(); //Aqui va el inicio de sesion de Carter
 		}
 		else if(opc=='2'){
 			printf("Te estamos esperando, Carter...");
 		}
 		else if(opc=='3'){
 			exit(0);
-		}			
+		}
 	}
+}
+
+void creditos(){
+	char c=' ';
+	printf("SERPIENTES Y ESCALERAS DELUXE EDITION SEASON PASS");
+	printf("\nCreado por:\nJorge Goytia\nAndres Lucatero\nDiego Mendez\nCassiel Ortega\nGabriel Villanueva");
+	printf("\n\n\n\n2017(c)\nPresiona enter para continuar");
+	while(c==' '){
+		scanf("%c",&c);
+	}
+	menuprin();
+}
+
+int main()
+{
+		creditos();
+		return 0;
 }
--- Tablero.c
+++ Tablero.c
@@ -1,33 +1,33 @@
 #include <stdio.h>
-#include <cstdlib>
+#include <stdlib.h>
 #include <time.h>
 
 int dados();
 
-main(){
+int main(){
     int pos=0,n;
-    bool band=true;
+    int band=1;
     srand(time(NULL));
-    
+
     char tablero[10][10];
-    
+
     for(int i=0;i<10;i++){
         for(int j=0;j<10;j++){
             tablero[i][j]=0;
         }
     }
-    
-    
-    
-    while(band==true){
+
+
+
+    while(band==1){
         pos+=dados();
         if(pos>=100){
-            band=false;
+            band=0;
             printf("\n¡Ganaste!");
-        } 
-        
+        }
+
         else{
-        
+
         printf("Estás es %d\n",pos);
         n=pos;
         int m=0;
@@ -39,16 +39,16 @@ main(){
             n=9;m++;
         }
         tablero[9-m][n-1]=1;
-        
+
         for(int i=0;i<10;i++){
             for(int j=0;j<10;j++){
                 printf("%d\t",tablero[i][j]);
         }
         printf("\n");
     }}
-           
-}
 
+}
+return 0;
 }
 
 
